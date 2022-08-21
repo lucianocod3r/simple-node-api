b4a55fd6ff5764022cb91a5a21d62764ea5fe2b2
lucianocod3r <luciano.cod3r@gmail.com>
2022-08-21 00:30:25 -0300

    fix: copy right simble

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 8000;
 const app = express();
 
 app.get('/', (req, res) => {
-   res.send('Hello World!<br><p>este é um teste de CI (continuous Integration) using github and a heroku app</p><br><br><br><p>copy;luciano.coder</p>');
+   res.send('Hello World!<br><p>este é um teste de CI (continuous Integration) using github and a heroku app</p><br><br><br><p>Luciano.Cod3r © 2022</p>');
 });
 
 app.listen(port, () => { console.log('Our App Is Up And Running!'); });
